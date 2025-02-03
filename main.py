a5691f4f8bcb39502c9d4dc3407d18d883659a85
Callen Shutts <callenshutts@gmail.com>
2025-02-03 18:28:10 -0500

    init commit with example from katago

---
+++ main.py
@@ -0,0 +1,106 @@
+import json
+import subprocess
+import time
+from threading import Thread
+import sgfmill
+import sgfmill.boards
+import sgfmill.ascii_boards
+from typing import Tuple, List, Optional, Union, Literal, Any, Dict
+
+Color = Union[Literal["b"],Literal["w"]]
+Move = Union[None,Literal["pass"],Tuple[int,int]]
+
+def sgfmill_to_str(move: Move) -> str:
+    if move is None:
+        return "pass"
+    if move == "pass":
+        return "pass"
+    (y,x) = move
+    return "ABCDEFGHJKLMNOPQRSTUVWXYZ"[x] + str(y+1)
+
+class KataGo:
+
+    def __init__(self, katago_path: str, config_path: str, model_path: str, additional_args: List[str] = []):
+        self.query_counter = 0
+        katago = subprocess.Popen(
+            [katago_path, "analysis", "-config", config_path, "-model", model_path, *additional_args],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        self.katago = katago
+        def printforever():
+            while katago.poll() is None:
+                data = katago.stderr.readline()
+                time.sleep(0)
+                if data:
+                    print("KataGo: ", data.decode(), end="")
+            data = katago.stderr.read()
+            if data:
+                print("KataGo: ", data.decode(), end="")
+        self.stderrthread = Thread(target=printforever)
+        self.stderrthread.start()
+
+    def close(self):
+        self.katago.stdin.close()
+
+
+    def query(self, initial_board: sgfmill.boards.Board, moves: List[Tuple[Color,Move]], komi: float, max_visits=None):
+        query = {}
+
+        query["id"] = str(self.query_counter)
+        self.query_counter += 1
+
+        query["moves"] = [(color,sgfmill_to_str(move)) for color, move in moves]
+        query["initialStones"] = []
+        for y in range(initial_board.side):
+            for x in range(initial_board.side):
+                color = initial_board.get(y,x)
+                if color:
+                    query["initialStones"].append((color,sgfmill_to_str((y,x))))
+        query["rules"] = "Chinese"
+        query["komi"] = komi
+        query["boardXSize"] = initial_board.side
+        query["boardYSize"] = initial_board.side
+        query["includePolicy"] = True
+        if max_visits is not None:
+            query["maxVisits"] = max_visits
+        return self.query_raw(query)
+
+    def query_raw(self, query: Dict[str,Any]):
+        self.katago.stdin.write((json.dumps(query) + "\n").encode())
+        self.katago.stdin.flush()
+
+        # print(json.dumps(query))
+
+        line = ""
+        while line == "":
+            if self.katago.poll():
+                time.sleep(1)
+                raise Exception("Unexpected katago exit")
+            line = self.katago.stdout.readline()
+            line = line.decode().strip()
+            # print("Got: " + line)
+        response = json.loads(line)
+
+        # print(response)
+        return response
+
+#download and place katago in a subfolder called katago and or change the path to the executable
+katago = KataGo('katago/katago.exe', 'katago/default_gtp.cfg', 'katago/kata1-b28c512nbt-s8268121856-d4612191185.bin.gz')
+
+board = sgfmill.boards.Board(19)
+komi = 6.5
+moves = [("b",(3,3))]
+
+displayboard = board.copy()
+for color, move in moves:
+    if move != "pass":
+        row,col = move
+        displayboard.play(row,col,color)
+print(sgfmill.ascii_boards.render_board(displayboard))
+
+print("Query result: ")
+print(katago.query(board, moves, komi))
+
+katago.close()
